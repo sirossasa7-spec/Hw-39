f34ee2baa793b6bbbba34d9e57e4e2524b3d05e6
sirossasa7@gmail.com <sirossasa7@gmail.com>
2026-06-05 16:09:37 +0200

    Request.h

--- Hw-39/Request.h
+++ Hw-39/Request.h
@@ -1,5 +1,21 @@
 #pragma once
-class Request
-{
+#include <string>
+
+class Request {
+private:
+    static int counter;
+
+public:
+    int id;
+    std::string destination;
+    std::string passenger;
+    std::string departureDate;
+
+    Request(const std::string& dest,
+        const std::string& pass,
+        const std::string& date);
 };
 
+struct RequestCompare {
+    bool operator()(const Request& a, const Request& b) const;
+};
