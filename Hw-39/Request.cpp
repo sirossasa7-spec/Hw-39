d9702fdbb0c13a1b5a81d08a9105f82b573f3391
sirossasa7@gmail.com <sirossasa7@gmail.com>
2026-06-05 20:11:00 +0200

    Request.cpp

--- Hw-39/Request.cpp
+++ Hw-39/Request.cpp
@@ -1,1 +1,25 @@
 #include "Request.h"
+
+int Request::counter = 0;
+
+Request::Request(const std::string& dest,
+    const std::string& pass,
+    const std::string& date)
+    : destination(dest),
+    passenger(pass),
+    departureDate(date)
+{
+    id = ++counter;
+}
+
+bool RequestCompare::operator()(const Request& a,
+    const Request& b) const
+{
+    if (a.destination != b.destination)
+        return a.destination < b.destination;
+
+    if (a.departureDate != b.departureDate)
+        return a.departureDate < b.departureDate;
+
+    return a.id < b.id;
+}
