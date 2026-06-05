2b8874a75dd9921b075a79ee0faa96a4da5b0108
sirossasa7@gmail.com <sirossasa7@gmail.com>
2026-06-05 20:46:27 +0200

    main

--- Hw-39/Hw-39.cpp
+++ Hw-39/Hw-39.cpp
@@ -1,20 +1,34 @@
-// Hw-39.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include "RequestManager.h"
+
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    RequestManager manager;
+    int choice;
+
+    do
+    {
+        cout << "\n===== МЕНЮ =====\n";
+        cout << "1. Додати заявку\n";
+        cout << "2. Видалити заявку\n";
+        cout << "3. Вивести заявки за пунктом призначення\n";
+        cout << "4. Порахувати заявки\n";
+        cout << "5. Вивести всі заявки\n";
+        cout << "0. Вихід\n";
+
+        cin >> choice;
+
+        switch (choice)
+        {
+        case 1: manager.addRequest(); break;
+        case 2: manager.deleteRequest(); break;
+        case 3: manager.showByDestination(); break;
+        case 4: manager.countRequests(); break;
+        case 5: manager.showAll(); break;
+        }
+    } while (choice != 0);
+
+    return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
